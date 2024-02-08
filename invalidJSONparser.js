73f4ba5e5e403fa3cf2c6dcb5ca72d00d5a9938e
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-02-08 18:43:07 +0200

    Update invalidJSONparser.js

--- invalidJSONparser.js
+++ invalidJSONparser.js
@@ -6,13 +6,19 @@ class InvalidJSONParser {
   parse(str) {
     
     let objNestingCounter = 0;
-    let lastValidIndex = (str.length - 1);
+    let lastValidIndex;
     
     for (let i = 0; i < str.length; i++) {
       
       const char = str[i];
       
       if (char === '{') {
+        
+        if (objNestingCounter === 0) {
+          
+          lastValidIndex = (i - 1);
+          
+        }
         
         objNestingCounter++;
         
@@ -21,12 +27,6 @@ class InvalidJSONParser {
       if (char === '}') {
         
         objNestingCounter--;
-        
-        if (objNestingCounter === 0) {
-          
-          lastValidIndex = i;
-          
-        }
         
       }
       
@@ -41,10 +41,17 @@ class InvalidJSONParser {
       // get the string up to the last valid index
       validStr = str.slice(0, lastValidIndex);
       
+    } else {
+      
+      lastValidIndex = (str.length - 1);
+      
     }
     
     
     // if there's an unclosed top-level array
+    
+    validStr = validStr.trim();
+    
     if (validStr.startsWith('[') &&
         !validStr.endsWith(']')) {
       
