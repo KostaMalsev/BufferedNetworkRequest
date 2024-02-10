a36697f1084354f75c0a5a93fca345f7c405ae08
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-02-10 15:50:18 +0200

    Update fetchCacheTest.js

--- fetchCacheTest.js
+++ fetchCacheTest.js
@@ -1,2 +1,31 @@
 
-
+<style>
+body {
+  font-family: system-ui;
+}
+</style>
+
+<div class="resp"></div>
+
+<script>
+
+const respEl = document.querySelector('.resp');
+
+async function main() {
+
+  const request = fetch('https://api.github.com/user/repos?visibility=all&sort=pushed&per_page=100&page=1', {
+    headers: {
+      authorization: 'token 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'
+    },
+    cache: 'no-store',
+    method: 'GET'
+  });
+  
+  respEl.textContent = await request.text();
+  
+}
+
+main();
+
+</script>
+
