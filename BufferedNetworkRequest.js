7043409cb3cb9f1a6c7afb4ea1c6919553495e5c
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-02-26 20:09:22 +0200

    Update BufferedNetworkRequest.js

--- BufferedNetworkRequest.js
+++ BufferedNetworkRequest.js
@@ -1,5 +1,5 @@
 
-async function BufferedNetworkRequest(request, updateCallback) {
+async function BufferedNetworkRequest(request, options = {}) {
   
   const response = await request;
   
@@ -18,7 +18,7 @@ async function BufferedNetworkRequest(request, updateCallback) {
         
         if (done) {
           
-          updateCallback(null, done);
+          callDoneCb();
           
           break;
           
@@ -30,11 +30,7 @@ async function BufferedNetworkRequest(request, updateCallback) {
         respText += string;
         
         
-        const parser = BufferedNetworkRequest.InvalidJSONParser;
-        
-        const validData = parser.parse(respText);
-        
-        updateCallback(validData);
+        callUpdateCb(string);
         
       }
       
@@ -43,6 +39,77 @@ async function BufferedNetworkRequest(request, updateCallback) {
     }
     
   }));
+  
+  
+  let lastDataLen = 0;
+  
+  function parseInvalidJSON(data) {
+    
+    const parser = BufferedNetworkRequest.InvalidJSONParser;
+    
+    data = parser.parse(respText);
+    
+    
+    const dataLen = data.length;
+    
+    // remove already-received data
+    // from array
+    data = data.slice(lastDataLen);
+    
+    lastDataLen = dataLen;
+    
+    
+    return data;
+    
+  }
+  
+  
+  let updateCb, doneCb;
+  
+  function callUpdateCb(data) {
+    
+    if (!updateCb) return;
+    
+    if (options.json) {
+      
+      return parseInvalidJSON(respText);
+      
+    }
+    
+    return data;
+    
+  }
+  
+  function callDoneCb() {
+    
+    if (!doneCb) return;
+    
+    if (options.json) {
+      
+      return JSON.parse(respText);
+      
+    }
+    
+    return respText;
+    
+  }
+  
+  
+  return {
+    
+    set onupdate(value) {
+      
+      updateCb = value;
+      
+    },
+    
+    set ondone(value) {
+      
+      doneCb = value;
+      
+    }
+    
+  };
 
 }
 
