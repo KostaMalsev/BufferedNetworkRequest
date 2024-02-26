7048a1d567343afaef1fad182cf24baf780d448f
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-02-26 20:48:49 +0200

    Update BufferedNetworkRequest.js

--- BufferedNetworkRequest.js
+++ BufferedNetworkRequest.js
@@ -10,7 +10,7 @@
  * request.ondone = (resp) => {}
  *
  * Options:
- * - json [boolean]: parse response data as a JSON array. false by default.
+ * - json [boolean]: parse response data as an array of JSON objects. false by default.
  *
  */
 
